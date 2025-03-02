3f4e380f836fdb41abcd5d92d251e3ea65c2b73f
Kanin Gleb <gleb.kanin@gmail.com>
2025-03-03 01:35:54 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import {User} from './06-callbacks/06.tsx';
+
+function App() {
+    return (
+        <div className="App">
+            <User/>
+        </div>
+    )
+}
+
+export default App;
