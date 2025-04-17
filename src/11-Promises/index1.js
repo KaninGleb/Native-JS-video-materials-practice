a55bdc7f0210e1d7114e45faee49ce7fe7f1581e
Kanin Gleb <gleb.kanin@gmail.com>
2025-04-17 16:49:28 +0300

    Promises - index.js renamed to index1.js

--- src/11-Promises/index1.js
+++ src/11-Promises/index1.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+// import axios from "axios";
 
 const users = [
   { id: 1, name: 'Dima', vacancies: 12 },
@@ -17,7 +17,7 @@ promise1
     //console.log(data)
   })
 
-const promise2 = findUserInDB(1)
+const promise2 = findUserInDB(10)
 promise2
   .then((user) => {
     //console.log(user)
@@ -31,17 +31,16 @@ otherPromise1
   .then((response) => {
     const dataFromGoogle = response[0]
     const userFromDB = response[1]
-    console.log(dataFromGoogle.data.vacancies + ' ; ' + userFromDB.name)
+    // console.log(dataFromGoogle.data.vacancies + ' ; ' + userFromDB.name)
   })
   .catch((error) => {
-    console.log('Initialization failed. Try later:', error)
+    // console.log('Initialization failed. Try later:', error)
   })
 
 otherPromise2
   .then((results) => {
-    console.log(results)
+    // console.log('.allSettled:', results)
     const dataFromGoogle = results[0].status === 'fulfilled' ? results[0].value : null
     const userFromDB = results[1].value
-    console.log(dataFromGoogle.data.vacancies + ' ; ' + userFromDB.name)
+    // console.log(dataFromGoogle.data.vacancies + ' ; ' + userFromDB.name)
   })
-
