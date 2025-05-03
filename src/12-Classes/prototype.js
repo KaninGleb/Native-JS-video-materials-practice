8c902cc8083e1e0ea8a59e65ecbb94bc62cf5f96
Kanin Gleb <gleb.kanin@gmail.com>
2025-05-03 18:14:24 +0300

    User prototype added

--- src/12-Classes/prototype.js
+++ src/12-Classes/prototype.js
@@ -1,4 +1,4 @@
-
+// 1 -----------------------------------------------
 function DeleteUserAction(userId) {
   this.type = 'DELETE-USER'
   this.payload = {
@@ -11,3 +11,21 @@ const action2 = new DeleteUserAction(333)
 
 // console.log(action1)
 // console.log(action2)
+
+
+// 2 -----------------------------------------------
+function User(name, site, dateOfBirth) {
+  this.name = name
+  this.site = site
+  this.dateOfBirth = dateOfBirth
+}
+
+User.prototype.hello = function () {
+  console.log(`I am ${this.name} from ${this.site}`)
+}
+
+const u1 = new User('Dimych', 'it-incubator', new Date(1900, 1, 2))
+const u2 = new User('Artem', 'it-incubator', new Date(1900, 10, 12))
+
+// u1.hello()
+// u2.hello()
