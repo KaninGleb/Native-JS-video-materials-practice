3e76387ef92c386bbf5d39390b8125ef519a4b40
Kanin Gleb <gleb.kanin@gmail.com>
2025-05-03 18:08:03 +0300

    DeleteUserAction class added

---
+++ src/12-Classes/class.js
@@ -0,0 +1,15 @@
+
+class DeleteUserAction {
+  constructor (userId) {
+    this.type = 'DELETE-USER'
+    this.payload = {
+      userId: userId
+    }
+  }
+}
+
+const action1 = new DeleteUserAction(121212)
+const action2 = new DeleteUserAction(333)
+
+// console.log(action1)
+// console.log(action2)
