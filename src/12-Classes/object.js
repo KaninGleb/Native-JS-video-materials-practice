21176c440fa6168a2258a9533ad62b0b0e1a54ad
Kanin Gleb <gleb.kanin@gmail.com>
2025-05-03 18:14:07 +0300

    commonHello object added

--- src/12-Classes/object.js
+++ src/12-Classes/object.js
@@ -1,3 +1,4 @@
+// 1 -----------------------------------------------
 const deleteUser = (userId) => {
   const action = {
     type: 'DELETE-USER',
@@ -13,3 +14,25 @@ const action2 = deleteUser('444444')
 
 // console.log(action1)
 // console.log(action2)
+
+
+// 2 -----------------------------------------------
+function commonHello() {
+  console.log(`I am ${this.name} from ${this.site}`)
+}
+
+const userFabric = (name) => {
+  const user = {
+    name,
+    site: 'it-incubator',
+    dateOfBirth: new Date(1900, 1, 2),
+    hello: commonHello
+  }
+  return user
+}
+
+const u1 = userFabric('Dimych')
+const u2 = userFabric('Artem')
+
+// u1.hello()
+// u2.hello()
